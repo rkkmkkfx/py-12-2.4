bdf2b5761ce3178989e70cf39b14d4f55ad88b46
rkkmkkfx(Baranov Sergei) <baranovs-v@mail.ru>
2018-03-27 04:19:27 +0800

    Done!

--- script.py
+++ script.py
@@ -3,16 +3,23 @@ import os
 path_name = 'Migrations'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
+
+def get_files_with_str(string, files):
+    founded = []
+    for file in files:
+        with open(os.path.join(migrations_dir, file)) as f:
+            data = f.read()
+            if string in data:
+                founded.append(migration)
+    return founded
+
+
 if __name__ == '__main__':
     migrations_dir = os.path.join(current_dir, path_name)
     migrations = [filename for filename in os.listdir(migrations_dir) if filename.endswith('.sql')]
     while True:
-        founded = []
         query = input('Введите строку: ')
+        migrations = get_files_with_str(query, migrations)
         for migration in migrations:
-            with open(os.path.join(migrations_dir, migration)) as f:
-                data = f.read()
-                if query in data:
-                    print(migration)
-                    founded.append(migration)
-        print('Всего: ', len(founded))
+            print(migration)
+        print('Всего: ', len(migrations))
